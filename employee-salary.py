8dbd6ac0c127e4a5a24963450dc858d093723e48
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2022-12-29 13:55:46 -0600

    First commit

---
+++ employee-salary.py
@@ -0,0 +1,7 @@
+'''
+The company knows the salary and the labor old of each employee
+Develop a program that reads those data and do:
+    *If the salary is less than 500 and his labor old is equal or higuer than 10 years, give him a 20% salary increase
+    *If the salary is less than 500, but his laabor old is less than 10 years, give him a 5% salary increase
+    *If the salary is higher or equal to 500 , print the salary without changes
+'''
